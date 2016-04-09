e79f2f9d3f83c4f4e7ab9a8a7309fe3c77c1d446
gehrigkeane <gehrigkeane@gmail.com>
2016-04-09 14:19:46 -0500

    Lets make the formatting barable

--- libpriqueue/libpriqueue.c
+++ libpriqueue/libpriqueue.c
@@ -8,14 +8,14 @@
 
 
 /**
-  Initializes the priqueue_t data structure.
-  
-  Assumtions
-    - You may assume this function will only be called once per instance of priqueue_t
-    - You may assume this function will be the first function called using an instance of priqueue_t.
-  @param q a pointer to an instance of the priqueue_t data structure
-  @param comparer a function pointer that compares two elements.
-  See also @ref comparer-page
+	Initializes the priqueue_t data structure.
+	
+	Assumtions
+		- You may assume this function will only be called once per instance of priqueue_t
+		- You may assume this function will be the first function called using an instance of priqueue_t.
+	@param q a pointer to an instance of the priqueue_t data structure
+	@param comparer a function pointer that compares two elements.
+	See also @ref comparer-page
  */
 void priqueue_init(priqueue_t *q, int(*comparer)(const void *, const void *))
 {
@@ -24,11 +24,11 @@ void priqueue_init(priqueue_t *q, int(*comparer)(const void *, const void *))
 
 
 /**
-  Inserts the specified element into this priority queue.
+	Inserts the specified element into this priority queue.
 
-  @param q a pointer to an instance of the priqueue_t data structure
-  @param ptr a pointer to the data to be inserted into the priority queue
-  @return The zero-based index where ptr is stored in the priority queue, where 0 indicates that ptr was stored at the front of the priority queue.
+	@param q a pointer to an instance of the priqueue_t data structure
+	@param ptr a pointer to the data to be inserted into the priority queue
+	@return The zero-based index where ptr is stored in the priority queue, where 0 indicates that ptr was stored at the front of the priority queue.
  */
 int priqueue_offer(priqueue_t *q, void *ptr)
 {
@@ -37,12 +37,12 @@ int priqueue_offer(priqueue_t *q, void *ptr)
 
 
 /**
-  Retrieves, but does not remove, the head of this queue, returning NULL if
-  this queue is empty.
+	Retrieves, but does not remove, the head of this queue, returning NULL if
+	this queue is empty.
  
-  @param q a pointer to an instance of the priqueue_t data structure
-  @return pointer to element at the head of the queue
-  @return NULL if the queue is empty
+	@param q a pointer to an instance of the priqueue_t data structure
+	@return pointer to element at the head of the queue
+	@return NULL if the queue is empty
  */
 void *priqueue_peek(priqueue_t *q)
 {
@@ -51,12 +51,12 @@ void *priqueue_peek(priqueue_t *q)
 
 
 /**
-  Retrieves and removes the head of this queue, or NULL if this queue
-  is empty.
+	Retrieves and removes the head of this queue, or NULL if this queue
+	is empty.
  
-  @param q a pointer to an instance of the priqueue_t data structure
-  @return the head of this queue
-  @return NULL if this queue is empty
+	@param q a pointer to an instance of the priqueue_t data structure
+	@return the head of this queue
+	@return NULL if this queue is empty
  */
 void *priqueue_poll(priqueue_t *q)
 {
@@ -65,13 +65,13 @@ void *priqueue_poll(priqueue_t *q)
 
 
 /**
-  Returns the element at the specified position in this list, or NULL if
-  the queue does not contain an index'th element.
+	Returns the element at the specified position in this list, or NULL if
+	the queue does not contain an index'th element.
  
-  @param q a pointer to an instance of the priqueue_t data structure
-  @param index position of retrieved element
-  @return the index'th element in the queue
-  @return NULL if the queue does not contain the index'th element
+	@param q a pointer to an instance of the priqueue_t data structure
+	@param index position of retrieved element
+	@return the index'th element in the queue
+	@return NULL if the queue does not contain the index'th element
  */
 void *priqueue_at(priqueue_t *q, int index)
 {
@@ -80,13 +80,13 @@ void *priqueue_at(priqueue_t *q, int index)
 
 
 /**
-  Removes all instances of ptr from the queue. 
-  
-  This function should not use the comparer function, but check if the data contained in each element of the queue is equal (==) to ptr.
+	Removes all instances of ptr from the queue. 
+	
+	This function should not use the comparer function, but check if the data contained in each element of the queue is equal (==) to ptr.
  
-  @param q a pointer to an instance of the priqueue_t data structure
-  @param ptr address of element to be removed
-  @return the number of entries removed
+	@param q a pointer to an instance of the priqueue_t data structure
+	@param ptr address of element to be removed
+	@return the number of entries removed
  */
 int priqueue_remove(priqueue_t *q, void *ptr)
 {
@@ -95,13 +95,13 @@ int priqueue_remove(priqueue_t *q, void *ptr)
 
 
 /**
-  Removes the specified index from the queue, moving later elements up
-  a spot in the queue to fill the gap.
+	Removes the specified index from the queue, moving later elements up
+	a spot in the queue to fill the gap.
  
-  @param q a pointer to an instance of the priqueue_t data structure
-  @param index position of element to be removed
-  @return the element removed from the queue
-  @return NULL if the specified index does not exist
+	@param q a pointer to an instance of the priqueue_t data structure
+	@param index position of element to be removed
+	@return the element removed from the queue
+	@return NULL if the specified index does not exist
  */
 void *priqueue_remove_at(priqueue_t *q, int index)
 {
@@ -110,10 +110,10 @@ void *priqueue_remove_at(priqueue_t *q, int index)
 
 
 /**
-  Returns the number of elements in the queue.
+	Returns the number of elements in the queue.
  
-  @param q a pointer to an instance of the priqueue_t data structure
-  @return the number of elements in the queue
+	@param q a pointer to an instance of the priqueue_t data structure
+	@return the number of elements in the queue
  */
 int priqueue_size(priqueue_t *q)
 {
@@ -122,9 +122,9 @@ int priqueue_size(priqueue_t *q)
 
 
 /**
-  Destroys and frees all the memory associated with q.
-  
-  @param q a pointer to an instance of the priqueue_t data structure
+	Destroys and frees all the memory associated with q.
+	
+	@param q a pointer to an instance of the priqueue_t data structure
  */
 void priqueue_destroy(priqueue_t *q)
 {
--- libscheduler/libscheduler.c
+++ libscheduler/libscheduler.c
@@ -10,9 +10,9 @@
 
 
 /**
-  Stores information making up a job to be scheduled including any statistics.
+	Stores information making up a job to be scheduled including any statistics.
 
-  You may need to define some global variables or a struct to store your job queue elements. 
+	You may need to define some global variables or a struct to store your job queue elements. 
 */
 typedef struct _job_t
 {
@@ -21,16 +21,16 @@ typedef struct _job_t
 
 
 /**
-  Initalizes the scheduler.
+	Initalizes the scheduler.
  
-  Assumptions:
-    - You may assume this will be the first scheduler function called.
-    - You may assume this function will be called once once.
-    - You may assume that cores is a positive, non-zero number.
-    - You may assume that scheme is a valid scheduling scheme.
+	Assumptions:
+		- You may assume this will be the first scheduler function called.
+		- You may assume this function will be called once once.
+		- You may assume that cores is a positive, non-zero number.
+		- You may assume that scheme is a valid scheduling scheme.
 
-  @param cores the number of cores that is available by the scheduler. These cores will be known as core(id=0), core(id=1), ..., core(id=cores-1).
-  @param scheme  the scheduling scheme that should be used. This value will be one of the six enum values of scheme_t
+	@param cores the number of cores that is available by the scheduler. These cores will be known as core(id=0), core(id=1), ..., core(id=cores-1).
+	@param scheme  the scheduling scheme that should be used. This value will be one of the six enum values of scheme_t
 */
 void scheduler_start_up(int cores, scheme_t scheme)
 {
@@ -39,23 +39,23 @@ void scheduler_start_up(int cores, scheme_t scheme)
 
 
 /**
-  Called when a new job arrives.
+	Called when a new job arrives.
  
-  If multiple cores are idle, the job should be assigned to the core with the
-  lowest id.
-  If the job arriving should be scheduled to run during the next
-  time cycle, return the zero-based index of the core the job should be
-  scheduled on. If another job is already running on the core specified,
-  this will preempt the currently running job.
-  Assumptions:
-    - You may assume that every job wil have a unique arrival time.
+	If multiple cores are idle, the job should be assigned to the core with the
+	lowest id.
+	If the job arriving should be scheduled to run during the next
+	time cycle, return the zero-based index of the core the job should be
+	scheduled on. If another job is already running on the core specified,
+	this will preempt the currently running job.
+	Assumptions:
+		- You may assume that every job wil have a unique arrival time.
 
-  @param job_number a globally unique identification number of the job arriving.
-  @param time the current time of the simulator.
-  @param running_time the total number of time units this job will run before it will be finished.
-  @param priority the priority of the job. (The lower the value, the higher the priority.)
-  @return index of core job should be scheduled on
-  @return -1 if no scheduling changes should be made. 
+	@param job_number a globally unique identification number of the job arriving.
+	@param time the current time of the simulator.
+	@param running_time the total number of time units this job will run before it will be finished.
+	@param priority the priority of the job. (The lower the value, the higher the priority.)
+	@return index of core job should be scheduled on
+	@return -1 if no scheduling changes should be made. 
  
  */
 int scheduler_new_job(int job_number, int time, int running_time, int priority)
@@ -65,18 +65,18 @@ int scheduler_new_job(int job_number, int time, int running_time, int priority)
 
 
 /**
-  Called when a job has completed execution.
+	Called when a job has completed execution.
  
-  The core_id, job_number and time parameters are provided for convenience. You may be able to calculate the values with your own data structure.
-  If any job should be scheduled to run on the core free'd up by the
-  finished job, return the job_number of the job that should be scheduled to
-  run on core core_id.
+	The core_id, job_number and time parameters are provided for convenience. You may be able to calculate the values with your own data structure.
+	If any job should be scheduled to run on the core free'd up by the
+	finished job, return the job_number of the job that should be scheduled to
+	run on core core_id.
  
-  @param core_id the zero-based index of the core where the job was located.
-  @param job_number a globally unique identification number of the job.
-  @param time the current time of the simulator.
-  @return job_number of the job that should be scheduled to run on core core_id
-  @return -1 if core should remain idle.
+	@param core_id the zero-based index of the core where the job was located.
+	@param job_number a globally unique identification number of the job.
+	@param time the current time of the simulator.
+	@return job_number of the job that should be scheduled to run on core core_id
+	@return -1 if core should remain idle.
  */
 int scheduler_job_finished(int core_id, int job_number, int time)
 {
@@ -85,17 +85,17 @@ int scheduler_job_finished(int core_id, int job_number, int time)
 
 
 /**
-  When the scheme is set to RR, called when the quantum timer has expired
-  on a core.
+	When the scheme is set to RR, called when the quantum timer has expired
+	on a core.
  
-  If any job should be scheduled to run on the core free'd up by
-  the quantum expiration, return the job_number of the job that should be
-  scheduled to run on core core_id.
+	If any job should be scheduled to run on the core free'd up by
+	the quantum expiration, return the job_number of the job that should be
+	scheduled to run on core core_id.
 
-  @param core_id the zero-based index of the core where the quantum has expired.
-  @param time the current time of the simulator. 
-  @return job_number of the job that should be scheduled on core cord_id
-  @return -1 if core should remain idle
+	@param core_id the zero-based index of the core where the quantum has expired.
+	@param time the current time of the simulator. 
+	@return job_number of the job that should be scheduled on core cord_id
+	@return -1 if core should remain idle
  */
 int scheduler_quantum_expired(int core_id, int time)
 {
@@ -104,11 +104,11 @@ int scheduler_quantum_expired(int core_id, int time)
 
 
 /**
-  Returns the average waiting time of all jobs scheduled by your scheduler.
+	Returns the average waiting time of all jobs scheduled by your scheduler.
 
-  Assumptions:
-    - This function will only be called after all scheduling is complete (all jobs that have arrived will have finished and no new jobs will arrive).
-  @return the average waiting time of all jobs scheduled.
+	Assumptions:
+		- This function will only be called after all scheduling is complete (all jobs that have arrived will have finished and no new jobs will arrive).
+	@return the average waiting time of all jobs scheduled.
  */
 float scheduler_average_waiting_time()
 {
@@ -117,11 +117,11 @@ float scheduler_average_waiting_time()
 
 
 /**
-  Returns the average turnaround time of all jobs scheduled by your scheduler.
+	Returns the average turnaround time of all jobs scheduled by your scheduler.
 
-  Assumptions:
-    - This function will only be called after all scheduling is complete (all jobs that have arrived will have finished and no new jobs will arrive).
-  @return the average turnaround time of all jobs scheduled.
+	Assumptions:
+		- This function will only be called after all scheduling is complete (all jobs that have arrived will have finished and no new jobs will arrive).
+	@return the average turnaround time of all jobs scheduled.
  */
 float scheduler_average_turnaround_time()
 {
@@ -130,11 +130,11 @@ float scheduler_average_turnaround_time()
 
 
 /**
-  Returns the average response time of all jobs scheduled by your scheduler.
+	Returns the average response time of all jobs scheduled by your scheduler.
 
-  Assumptions:
-    - This function will only be called after all scheduling is complete (all jobs that have arrived will have finished and no new jobs will arrive).
-  @return the average response time of all jobs scheduled.
+	Assumptions:
+		- This function will only be called after all scheduling is complete (all jobs that have arrived will have finished and no new jobs will arrive).
+	@return the average response time of all jobs scheduled.
  */
 float scheduler_average_response_time()
 {
@@ -143,10 +143,10 @@ float scheduler_average_response_time()
 
 
 /**
-  Free any memory associated with your scheduler.
+	Free any memory associated with your scheduler.
  
-  Assumptions:
-    - This function will be the last function called in your library.
+	Assumptions:
+		- This function will be the last function called in your library.
 */
 void scheduler_clean_up()
 {
@@ -155,15 +155,15 @@ void scheduler_clean_up()
 
 
 /**
-  This function may print out any debugging information you choose. This
-  function will be called by the simulator after every call the simulator
-  makes to your scheduler.
-  In our provided output, we have implemented this function to list the jobs in the order they are to be scheduled. Furthermore, we have also listed the current state of the job (either running on a given core or idle). For example, if we have a non-preemptive algorithm and job(id=4) has began running, job(id=2) arrives with a higher priority, and job(id=1) arrives with a lower priority, the output in our sample output will be:
+	This function may print out any debugging information you choose. This
+	function will be called by the simulator after every call the simulator
+	makes to your scheduler.
+	In our provided output, we have implemented this function to list the jobs in the order they are to be scheduled. Furthermore, we have also listed the current state of the job (either running on a given core or idle). For example, if we have a non-preemptive algorithm and job(id=4) has began running, job(id=2) arrives with a higher priority, and job(id=1) arrives with a lower priority, the output in our sample output will be:
 
-    2(-1) 4(0) 1(-1)  
-  
-  This function is not required and will not be graded. You may leave it
-  blank if you do not find it useful.
+		2(-1) 4(0) 1(-1)  
+	
+	This function is not required and will not be graded. You may leave it
+	blank if you do not find it useful.
  */
 void scheduler_show_queue()
 {
